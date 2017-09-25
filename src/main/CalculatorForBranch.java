8fc3974fc93b9b212386c94a4cd7f68ebd5fc6f9
李明翰 <chuck@liminghandeMacBook-Pro.local>
2017-09-25 16:35:13 +0800

    add master branch method

--- src/main/CalculatorForBranch.java
+++ src/main/CalculatorForBranch.java
@@ -13,8 +13,12 @@ public class CalculatorForBranch {
 	public static int multiply(int a, int b) {
 		return a * b;
 	}
-	
+
 	public static int div(int a, int b) {
+		return a / b;
+	}
+
+	public static int masterbranch(int a, int b) {
 		return a / b;
 	}
 }
